b7f9ff14bdc492e1727bc42d81a047eba7f21f53
MobileHorse <58142664+MobileHorse@users.noreply.github.com>
2021-05-07 03:51:10 +0300

    Phase A

--- assets/js/development-project-actions.js
+++ assets/js/development-project-actions.js
@@ -1,2 +1,12 @@
 $(document).ready(function () {
+    $(".close-popup").on("click", function () {
+        $("body").addClass("hidden-popup");
+    });
+    $(".open-popup").on("click", function () {
+        $("body").removeClass("hidden-popup");
+    });
+
+    $(".add-action").on("click", function () {
+        $("body").removeClass("hidden-popup");
+    });
 });
